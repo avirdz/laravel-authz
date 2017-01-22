0cbf831640507259c01d2b8604e4a202c532f097
Jesus Aviles <jesus.aviles@socialreality.com>
2017-01-22 15:56:23 -0800

    fix shareable table name

--- migrations/2017_01_21_052245_create_shareables_table.php
+++ migrations/2017_01_21_052245_create_shareables_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateSharedResourcesTable extends Migration
+class CreateShareablesTable extends Migration
 {
     /**
      * Run the migrations.
