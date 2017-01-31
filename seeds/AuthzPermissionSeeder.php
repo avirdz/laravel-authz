cec241242e0ce2514b0fb712104a076e2f155863
Jesus Aviles <jesus.aviles@socialreality.com>
2017-01-31 14:13:26 -0800

    fix defualt permission value for seedet

--- seeds/AuthzPermissionSeeder.php
+++ seeds/AuthzPermissionSeeder.php
@@ -16,77 +16,77 @@ class AuthzPermissionSeeder extends Seeder
             [
                 'key_name' => 'groups.view',
                 'description' => 'View groups',
-                'value' => Permission::DENY_ALL,
+                'value' => Permission::ROOT,
             ],
             [
                 'key_name' => 'groups.create',
                 'description' => 'Create a group',
-                'value' => Permission::DENY_ALL,
+                'value' => Permission::ROOT,
             ],
             [
                 'key_name' => 'groups.update',
                 'description' => 'Modify a group',
-                'value' => Permission::DENY_ALL,
+                'value' => Permission::ROOT,
             ],
             [
                 'key_name' => 'groups.delete',
                 'description' => 'Delete a group',
-                'value' => Permission::DENY_ALL,
+                'value' => Permission::ROOT,
             ],
             [
                 'key_name' => 'groups.add-user',
                 'description' => 'Add users to a group',
-                'value' => Permission::DENY_ALL,
+                'value' => Permission::ROOT,
             ],
             [
                 'key_name' => 'groups.remove-user',
                 'description' => 'Remove users from a group',
-                'value' => Permission::DENY_ALL,
+                'value' => Permission::ROOT,
             ],
             [
                 'key_name' => 'permissions.view',
                 'description' => 'View permissions',
-                'value' => Permission::DENY_ALL,
+                'value' => Permission::ROOT,
             ],
             [
                 'key_name' => 'permissions.create',
                 'description' => 'Create a permission',
-                'value' => Permission::DENY_ALL,
+                'value' => Permission::ROOT,
             ],
             [
                 'key_name' => 'permissions.update',
                 'description' => 'Modify a group',
-                'value' => Permission::DENY_ALL,
+                'value' => Permission::ROOT,
             ],
             [
                 'key_name' => 'permissions.delete',
                 'description' => 'Delete a permission',
-                'value' => Permission::DENY_ALL,
+                'value' => Permission::ROOT,
             ],
             [
                 'key_name' => 'permissions.grant',
                 'description' => 'Set permission granted to a user or group',
-                'value' => Permission::DENY_ALL,
+                'value' => Permission::ROOT,
             ],
             [
                 'key_name' => 'permissions.deny',
                 'description' => 'Set permission denied to a user or group',
-                'value' => Permission::DENY_ALL,
+                'value' => Permission::ROOT,
             ],
             [
                 'key_name' => 'shareables.share',
                 'description' => 'Share a resource with a user',
-                'value' => Permission::ONLY_ME,
+                'value' => Permission::ROOT,
             ],
             [
                 'key_name' => 'shareables.unshare',
                 'description' => 'Unshare a resource with a user',
-                'value' => Permission::ONLY_ME,
+                'value' => Permission::ROOT,
             ],
             [
                 'key_name' => 'shareables.deny',
                 'description' => 'Set permission denied to a user on a shared resource',
-                'value' => Permission::ONLY_ME,
+                'value' => Permission::ROOT,
             ],
         ]);
     }
